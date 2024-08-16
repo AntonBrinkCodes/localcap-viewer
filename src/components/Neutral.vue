8dac14304bbb22d1e64014c2bf1ee6edfd11b83b
Anton Brink <77839398+AntonBrinkCodes@users.noreply.github.com>
2024-08-16 13:04:13 +0200

    Create Neutral.vue

---
+++ src/components/Neutral.vue
@@ -0,0 +1,32 @@
+<template>
+
+
+
+</template>
+
+
+
+<script>
+import { ref } from 'vue'
+import {mapState, mapActions} from 'vuex';
+import MainLayout from '/src/layout/MainLayout.vue'
+
+
+export default {
+    name: 'Neutral',
+    components: {
+      MainLayout
+    }, 
+    data: () =>({
+
+    }),
+    computed: {
+        sessionID: state => state.sessionID
+        
+    }
+}
+
+
+
+
+</script>
